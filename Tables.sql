db88f5e117026ea84fa38d1f693c9d915d60691f
GeorgiaWatkins <160261340+GeorgiaWatkins@users.noreply.github.com>
2024-11-18 14:29:18 -0500

    Update Tables.sql

--- Tables.sql
+++ Tables.sql
@@ -1,12 +1,3 @@
-CREATE TABLE FACT_CLIENT (
-	ClientID int PRIMARY KEY,
-	ClientFN varchar(40) Not Null,
-	ClientLN varchar(40) Not Null,
-	ClientEmail varchar(40) Not Null,
-	ClientPhone int Not Null,
-	ClientBirthDate date Not Null,
-	ClientBackground boolean Not Null
-	);
 
 	
 CREATE TABLE FACT_EMPLOYEE (
@@ -14,11 +5,9 @@ CREATE TABLE FACT_EMPLOYEE (
 	EmpFN varchar(40) Not Null,
 	EmpLN varchar(40) Not Null,
 	EmpEmail varchar(40) Not Null,
-	EmpSSN int Not Null,
-	CenterID int Not Null,
-	Foreign Key (CenterID) References FACT_CENTER(CenterID),
+	EmpSSN int Not Null UNIQUE,
+	Foreign Key (CenterID) References FACT_CENTER(CenterID)
 	);
-
 
 CREATE TABLE FACT_CENTER (
 	CenterID int Primary Key,
@@ -29,48 +18,53 @@ CREATE TABLE FACT_CENTER (
 	CenterPhone int Not Null,
 	);
 
+CREATE TABLE FACT_EMPLOYEE (
+	EmpID int PRIMARY KEY,
+	EmpFN varchar(40) Not Null,
+	EmpLN varchar(40) Not Null,
+	EmpEmail varchar(40) Not Null,
+	EmpSSN int Not Null UNIQUE,
+    CenterID int NOT NULL,
+	Foreign Key (CenterID) References FACT_CENTER(CenterID)
+	);
 
 CREATE TABLE FACT_PET (
 	PetID int Primary Key,
 	PetBirthDate date Not Null,
 	PetName varchar(30) Not Null,
-	Foreign Key CenterID References FACT_CENTER(CenterID),
+    CenterID int NOT NULL,
+	Foreign Key (CenterID) References FACT_CENTER(CenterID)
 	);
-
 	
 CREATE TABLE FACT_BREED (
 	PetID int Not Null,
 	BreedType varchar(30) Not Null,
 	Primary Key (PetID, BreedType),
-	Foreign Key PetID References FACT_PET(PetID),
+	Foreign Key (PetID) References FACT_PET(PetID)
 	);
-
 
 CREATE TABLE FACT_DROPSOFF (
 	DropsOffDate date,
-    PetID SMALLINT,
-    ClientID SMALLINT,
+    PetID int,
+    ClientID int,
     FOREIGN KEY(PetID) REFERENCES FACT_PET(PetID),
     FOREIGN KEY(ClientID) REFERENCES FACT_CLIENT(ClientID),
     PRIMARY KEY(DropsOffDate, PetID, ClientID)
 	);
 
-
 CREATE TABLE FACT_ADOPTS (
 	AdoptionDate date,
-    PetID SMALLINT,
-    ClientID SMALLINT,
+    PetID int,
+    ClientID int,
     FOREIGN KEY(PetID) REFERENCES FACT_PET(PetID),
     FOREIGN KEY(ClientID) REFERENCES FACT_CLIENT(ClientID),
     PRIMARY KEY(AdoptionDate, PetID, ClientID)
 	);
 
-
 CREATE TABLE FACT_MEDHISTORY (
-    PetID SMALLINT,
+    PetID int,
    	MedicalReason VARCHAR(100),
-    VistNum SMALLINT,
+    VisitNum int,
     FOREIGN KEY(PetID) REFERENCES FACT_PET(PetID),
     PRIMARY KEY(PetID, MedicalReason, VisitNum)
-	);
-    
+	); 
